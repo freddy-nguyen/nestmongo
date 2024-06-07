3cef1a7e9a0fa0027e5dc9e548437784d8f855de
FREDDY NGUYEN <qacer6973@gmail.com>
2024-06-06 22:27:24 -0400

    resource users

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       }),
       inject: [ConfigService],
     }),
+    UsersModule,
     // MongooseModule.forRoot('mongodb+srv://qacer6973:Qetuo3.3.3.@cluster0.votkycs.mongodb.net/')
   ],
   controllers: [AppController],

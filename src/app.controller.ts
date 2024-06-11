c9c1c7584a0c55cd2545ee5c2d1a4189916233e6
FREDDY NGUYEN <qacer6973@gmail.com>
2024-06-11 13:04:56 -0400

    soft delete

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,28 @@
-import { Controller, Get, Post, Render, Request, UseGuards } from '@nestjs/common';
-import { AppService } from 'src/app.service';
-import { ConfigService } from '@nestjs/config';
-import { AuthGuard } from '@nestjs/passport';
+import { Body, Controller, Get, Post, Render, Request, UseGuards } from '@nestjs/common';
+// import { AppService } from 'src/app.service';
+// import { ConfigService } from '@nestjs/config';
+// import { AuthGuard } from '@nestjs/passport';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
+// import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { Public } from './decorator/customize';
+import { CreateUserDto } from './users/dto/create-user.dto';
+import { UsersService } from './users/users.service';
 
 @Controller()
 export class AppController {
   constructor(
-    private readonly appService: AppService,
-    private configService: ConfigService, // ENV
+    // private readonly appService: AppService,
+    // private configService: ConfigService, // ENV
     private authService: AuthService,
+    private usersService: UsersService,
   ) { }
+
+  @Public()
+  @Post('create')
+  createUser(@Body() users: CreateUserDto) {
+    return this.usersService.create(users)
+  }
 
   @Public()
   @UseGuards(LocalAuthGuard)
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { User } from './schemas/user.schema';
+import { User, UserDocument } from './schemas/user.schema';
 import mongoose, { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { genSaltSync, hashSync, compareSync } from 'bcryptjs'; //if library's fault, export their functions directly
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User.name) private userModel: Model<User>) { }
+  constructor(@InjectModel(User.name) private userModel: SoftDeleteModel<UserDocument>) { }
 
   hashPassword = (password: string) => {
     const salt = genSaltSync(10);
@@ -57,7 +58,7 @@ export class UsersService {
   async remove(id: string) {
     if (!mongoose.Types.ObjectId.isValid(id))
       return 'User not found.'
-    await this.userModel.deleteOne({ _id: id })
+    await this.userModel.softDelete({ _id: id })
     return 'Deleted successfully'
   }
 }
